c07bac8138bb0fb13b406ad1fe3c235befafd08a
opilane <opilane@HP-CZC2349HTF>
2023-12-21 12:39:34 +0200

    21.12.23

--- DavidMyrseth__TARpv23.py
+++ DavidMyrseth__TARpv23.py
@@ -1,7 +1,24 @@
-print("Tere maailm!")
-nimi=input("Mis on sinu nimi?") #comment
-vanus=int(input(Kui vana sa oled?)) #float()->2.5
-print("Tere tulemas! "+nimi+". Sa oled"+str(vanus)+" aastat vana")
-print("Tere tulemast! ",nimi,". Sa oled",vanus,"aasatat vana")
-print("Tere tulemast! {0}.Sa oled {1} aastat vana".format(nimi,vanus))
-print("Muutuja vanus",vanus,"tüüp on",type(vanus))
+#print("Tere maailm!")
+#nimi=input("Mis on sinu nimi?") #comment
+#vanus=int(input("Kui vana sa oled?")) #float()->2.5
+#print("Tere tulemas! "+nimi+". Sa oled"+str(vanus)+" aastat vana")
+#print("Tere tulemast! ",nimi,". Sa oled",vanus,"aasatat vana")
+#print("Tere tulemast! {0}.Sa oled {1} aastat vana".format(nimi,vanus))
+#print("Muutuja vanus",vanus,"tüüp on",type(vanus))
+#2
+#vanus= 18
+#eesnimi = "Jaak"
+#pikkus = 16.5
+#Kas käib kollis = True
+#print("Muutuja vanus",eesnimi,"tüüp on",type(eesnimi))
+#print("Muutuja vanus",pikkus,"tüüp on",type(pikkus))
+#print("Muutuja vanus",kas_käib_koolis,"tüüp on",type(kas_käib_koolis))
+#print("Muutuja vanus",pikkus,"tüüp on",type(pikkus))
+#3
+from random import *
+konfeti=randint(10,100)
+print("konfeti",konfeti)
+ukral=int(input("skolko ukral"))
+konfeti-=ukral
+print("na stole teper,konfeti","konfet")
+print()
