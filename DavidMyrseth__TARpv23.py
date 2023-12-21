36fc4818260d7a922dda43c123347dc0cf760ab4
opilane <opilane@HP-CZC2349HTF>
2023-12-21 12:38:54 +0200

    21.12.2023

--- DavidMyrseth__TARpv23.py
+++ DavidMyrseth__TARpv23.py
@@ -1,1 +1,7 @@
-print("Tere maail")
+print("Tere maailm!")
+nimi=input("Mis on sinu nimi?") #comment
+vanus=int(input(Kui vana sa oled?)) #float()->2.5
+print("Tere tulemas! "+nimi+". Sa oled"+str(vanus)+" aastat vana")
+print("Tere tulemast! ",nimi,". Sa oled",vanus,"aasatat vana")
+print("Tere tulemast! {0}.Sa oled {1} aastat vana".format(nimi,vanus))
+print("Muutuja vanus",vanus,"tüüp on",type(vanus))
