749ddc737e3d09a24f67b554f1608dbeeb68c9d3
David <opilane@HP-CZC2349HTF>
2024-01-11 12:14:33 +0200

    First work

--- DavidMyrseth__TARpv23.py
+++ DavidMyrseth__TARpv23.py
@@ -1,3 +1,4 @@
+#1
 #print("Tere maailm!")
 #nimi=input("Mis on sinu nimi?") #comment
 #vanus=int(input("Kui vana sa oled?")) #float()->2.5
@@ -15,10 +16,56 @@
 #print("Muutuja vanus",kas_käib_koolis,"tüüp on",type(kas_käib_koolis))
 #print("Muutuja vanus",pikkus,"tüüp on",type(pikkus))
 #3
+#from random import *
+#konfeti=randint(10,100)
+#print("konfeti",konfeti)
+#ukral=int(input("skolko ukral"))
+#konfeti-=ukral
+#print("na stole teper,konfeti","konfet")
+#print()
+#4
+#c = int(input("длинна окружности дерева"))
+#summa = c/3.14
+#print("диаметр окружности равен:", summa)
+#5
+#N = int(input("напишите первое число"))
+#M = int(input("напишите первое второе число"))
+#summa = N*M
+#print("диагонали прямоугольного участка земли равен", summa)
+#6
+#time = float (input ("Сколько часов ушло на поездку?") )
+#Road = float (input ("Сколько километров ты проехал?") )
+#speed = time/Road
+#print(speed)
+#7
+#numbers = (1,2,3,4,5)
+#summa = sum(numbers)/len(numbers)
+#print(summa)
+#8
+#frog ="""
+#    @..@
+#   (----)
+#  ( \__/ )
+#^^ "" ^^  
+#"""
+#9
+#a = int(input("переменная 1"))
+#b = int(input("переменная 2"))
+#c = int(input("переменная 3"))
+#summa = a+b+c
+#print("периметр треугольника:", summa)
+#10
+from importlib.resources import Package
 from random import *
-konfeti=randint(10,100)
-print("konfeti",konfeti)
-ukral=int(input("skolko ukral"))
-konfeti-=ukral
-print("na stole teper,konfeti","konfet")
-print()
+from tkinter import Pack
+#10
+Parandint(1,5)
+hind=12.90
+hind=1.1 #hind koos jätatega
+osa=hind/Package
+
+
+#Pitsa = 12.90
+#summa = Pitsa/10
+#money = (Pitsa+summa)/2
+#print(money)
