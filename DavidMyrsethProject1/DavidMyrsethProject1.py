c371427acc33c19ec159da8682c6d7e24b2da041
DavidMyrsethTARpv23 <david.mirsetSSS@gmail.com>
2024-01-19 11:43:29 +0200

    - brain 5

---
+++ DavidMyrsethProject1/DavidMyrsethProject1.py
@@ -0,0 +1,41 @@
+﻿#1
+#for
+#from ast import Break
+
+#r=0
+#for i in range(15):
+#    arv=float(input("Sisesta {0} arv".format(i+1)))
+#    if arv==int(arv):
+#       r+=1
+#print("Täisarvude arv on "+str(r))
+
+##while True
+#r=0
+#i=0
+#while True:
+#    i+=1
+#    arv=float(input("Sisesta {0} arv".format(i+1)))
+#    if arv==int(arv):
+#       r+=1
+#    if i==15: break
+#print("Täisarvude arv on "+str(r))
+
+#while tingimustega
+#r=0
+#i=0
+#while i<15:
+#    i+=1
+#    arv=float(input("Sisesta {0} arv".format(i+1)))
+#    if arv==int(arv):
+#       r+=1
+
+#print("Täisarvude arv on "+str(r))
+
+#2
+A=15
+for i in range(1,14,A):
+    arv=float(input("Sisesta {0} arv".format(i)))
+    if arv==int(arv):
+       A+=1
+print("Täisarvude arv on "+str(A))
+
